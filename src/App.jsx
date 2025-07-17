9a4303e9aff00a44b49d4a41ecb07fcc38186105
Aman Tailor <amantailorpw@Aman-12605.local>
2025-07-17 18:45:56 +0530

    feat: added loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import "./App.css";
 import Sidebar from "./Sidebar";
-import { Send } from "lucide-react";
+import { Send, Loader } from "lucide-react";
 import { GoogleGenAI } from "@google/genai";
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
 
 const API_KEY = import.meta.env.VITE_GEMINI_API_KEY;
 const ai = new GoogleGenAI({apiKey: API_KEY});
@@ -11,21 +13,31 @@ function App() {
   const [inputValue, setInputValue] = useState("");
   const [submitedText, setSubmitedText] = useState("");
   const [geminiResponse, setGeminiResponse] = useState("");
+  const [loading, setLoading] = useState(false);
 
 
   const handleSubmit = async () => {
     setSubmitedText(inputValue);
+    setGeminiResponse("");
+    setLoading(true); 
     setInputValue("");
-    const result = await ai.models.generateContent({
-      model: "gemini-2.5-flash",
-      contents: inputValue,
-    });
-    const text = result.text;
-    console.log(text)
-    setGeminiResponse(text);
+    try {
+      const result = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: inputValue,
+      });
+      const text = result.text;
+      setGeminiResponse(text);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setGeminiResponse("❌ Failed to fetch data.");
+    } finally {
+      setLoading(false);
+    }
     
   };
 
+  const markdown = geminiResponse;
   return (
     <>
       <div className="flex">
@@ -42,8 +54,17 @@ function App() {
                 <h1 className="absolute top-20 right-10 text-black px-4 py-2 ">
                   {submitedText}
                 </h1>
-                <div className="mt-50 outline-2 outline-gray-500/50 border  rounded-xl">
-                  <p className="p-10">{geminiResponse}</p>
+                <div className="mt-50 outline-2 outline-gray-500/50 border rounded-xl p-10 min-h-[200px]">
+                  {loading ? (
+                    <div className="flex justify-center items-center h-32 text-lg font-semibold text-gray-500">
+                    <Loader className="animate-spin h-32 " size={18}/>
+                     <p className="p-2">Loading...</p>
+                    </div>            
+                  ) : (
+                    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+                      {markdown}
+                    </ReactMarkdown>
+                  )}
                 </div>
               </div>
             )}
